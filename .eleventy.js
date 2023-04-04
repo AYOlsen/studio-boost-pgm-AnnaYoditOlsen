1b6adc11874d99745f254dc873cee7d0e418a89d
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-04-04 23:45:27 +0200

    Make index page

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ module.exports = (eleventyConfig) => {
     "./input/assets/fonts": "fonts/",
   });
 
+  const sliceFilter = (array, num1, num2) => {
+    return array.slice(num1, num2);
+  }
+
+  eleventyConfig.addFilter('sliceFilter', sliceFilter)
+
   return {
     dir: {
       input: "./input/pages/",
